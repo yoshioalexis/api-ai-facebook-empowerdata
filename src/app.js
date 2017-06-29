a5a1ece9775f09dfcd00f53698dec3e1970af9bc
Yoshio <yoshio@empowerdata.mx>
2017-06-28 20:48:23 -0500

    hola

--- src/app.js
+++ src/app.js
@@ -311,6 +311,7 @@ class FacebookBot {
             //         }
             //     });
             //
+            
 
             let apiaiRequest = apiAiService.textRequest(text,
             {
